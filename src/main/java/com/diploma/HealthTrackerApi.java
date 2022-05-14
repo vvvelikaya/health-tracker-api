40c15a31cabc48a01662b7a29509c8be852cbfe3
vvvelikaya <nik.velikay@gmail.com>
2022-05-14 23:26:31 +0300

    initial commit

---
+++ src/main/java/com/diploma/HealthTrackerApi.java
@@ -0,0 +1,11 @@
+package com.diploma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthTrackerApi {
+    public static void main(String[] args) {
+        SpringApplication.run(HealthTrackerApi.class, args);
+    }
+}
