db2cc4ff2f46e7fb027c868887ff09e6a10af0aa
vvvelikaya <nik.velikay@gmail.com>
2022-06-05 16:48:37 +0300

    edited migration V1

--- src/main/resources/db/migration/V1__create_user_table.sql
+++ src/main/resources/db/migration/V1__create_user_table.sql
@@ -1,8 +1,8 @@
-CREATE TABLE account (
+CREATE TABLE user_account (
     id bigserial PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     surname VARCHAR(50) NOT NULL,
-    email VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
     birth_date DATE,
     gender VARCHAR(6),
     weight INT
