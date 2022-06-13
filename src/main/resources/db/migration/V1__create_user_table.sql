0cb2de6fae9be4f26d5bdde2ac3807dcea25bf77
vvvelikaya <nik.velikay@gmail.com>
2022-06-13 14:45:21 +0300

    fixed migration

--- src/main/resources/db/migration/V1__create_user_table.sql
+++ src/main/resources/db/migration/V1__create_user_table.sql
@@ -3,7 +3,9 @@ CREATE TABLE user_account (
     name VARCHAR(50) NOT NULL,
     surname VARCHAR(50) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
     birth_date DATE,
     gender VARCHAR(6),
-    weight INT
+    weight INT,
+    role VARCHAR(20) DEFAULT 'ROLE_USER' NOT NULL
 );
