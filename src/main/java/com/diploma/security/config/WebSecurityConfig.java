e59d482adbedd3fdea060fa8ef111b2ba93e8628
vvvelikaya <nik.velikay@gmail.com>
2022-06-15 15:16:10 +0300

    added authorization filter

--- src/main/java/com/diploma/security/config/WebSecurityConfig.java
+++ src/main/java/com/diploma/security/config/WebSecurityConfig.java
@@ -1,8 +1,10 @@
 package com.diploma.security.config;
 
 import com.diploma.security.filter.CustomAuthenticationFilter;
+import com.diploma.security.filter.CustomAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,6 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.http.HttpMethod.GET;
 
@@ -28,10 +31,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-//        http.authorizeRequests().antMatchers(GET, "/api/users").hasAnyAuthority("ROLE_ADMIN");
-        http.authorizeRequests().anyRequest().permitAll();
-        http.addFilter(new CustomAuthenticationFilter(super.authenticationManager()));
+        http
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.authorizeRequests()
+                .antMatchers("/login").permitAll()
+                .antMatchers(GET, "api/users").hasAnyAuthority("ROLE_USER")
+                .anyRequest().authenticated();
+        http
+                .addFilter(new CustomAuthenticationFilter(super.authenticationManager()))
+                .addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 }
